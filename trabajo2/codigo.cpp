8a484cdc16aa36942b22bffefe9be717f49bc4a5
xJuliusDevex <jcesargarq22@gmail.com>
2022-08-31 00:24:16 -0500

    trabajo

---
+++ trabajo2/codigo.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <semaphore.h>
+#include <thread>
+#include <unistd.h>
+#include <stdlib.h>
+#define n 1
+/*------------Declaracion de los monitores-----------------*/
+class productor;
+class consumidor;   
+using namespace std;    
+class buffer{
+    private:
+    /*Variables*/
+    sem_t libres,mutex;
+    int cola[n],elemento,final=0,a=0;
+    public:
+    void insertar(int,int&);
+    int retirar(int,int&);
+};
+void buffer::insertar(int elemento,int &pos)
+{
+    
+    sleep(1);
+    while(cola[a]!=NULL&&a!=n)//busca la cola de espacios vacios
+    {
+        a++;
+    }
+
+    sem_wait(&libres);
+    if(cola[a]==NULL)
+    {
+        sem_wait(&libres);
+        final=a;
+        cola[final]=elemento;//Guardamos elemento a la cola en un espacio    
+        int i=final;
+    }
+    sleep(1);//esperar
+    sem_post(&mutex);//libera la sc   
+}
+int buffer::retirar(int elemento,int &pos){
+    sem_wait(&mutex);
+    int frente=0;
+    int a=0;
+    while(cola[a]!=elemento&& a!=n){
+        a++;    
+    }
+    sleep(1);
+    if(cola[a]==elemento)
+    {
+        frente=a;
+        elemento=cola[frente];
+        return=elemento;
+        int i=frente;   
+        sleep(1);
+    }
+    sem_post(&mutex);
+}
+void productoA()
+{
+
+}
